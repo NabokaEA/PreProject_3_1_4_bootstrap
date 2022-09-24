7970e16ffb559d735dfbfa2ec2465f7995a0f017
NabokaEA <nabokae@yandex.ru>
2022-09-24 16:14:10 +0300

    csrf

--- src/main/java/ru/nabokae/configs/WebSecurityConfig.java
+++ src/main/java/ru/nabokae/configs/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 .authorizeRequests()
                 .antMatchers("/Userhome","/login","/error").permitAll()
                 .anyRequest().authenticated()
