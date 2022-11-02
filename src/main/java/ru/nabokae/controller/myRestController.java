8075fe48269f3e4431a7f9db2d8e774879633036
NabokaEA <nabokae@yandex.ru>
2022-11-02 22:36:04 +0300

    Bootstrap 5 rest delete 6

--- src/main/java/ru/nabokae/controller/myRestController.java
+++ src/main/java/ru/nabokae/controller/myRestController.java
@@ -1,6 +1,8 @@
 package ru.nabokae.controller;
 
 import net.bytebuddy.implementation.bytecode.Throw;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api")
 public class myRestController {
+    public static final Logger logger = LoggerFactory.getLogger(myRestController.class);
     private final UserServiceImpl userService;
 
     @Autowired
@@ -38,6 +41,12 @@ public class myRestController {
     public User getUser(@PathVariable("id") Long id) {
         Optional<User> user = userService.findById(id);
         return user.orElseThrow(UserNotFoundException::new);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable("id") Long id) {
+        logger.info("Запрошен метод удаления REST");
+        userService.delete(id);
     }
 
     @PostMapping("/user")
